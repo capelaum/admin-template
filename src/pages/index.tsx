6d95b08e73c17544339af51fa1c04f7b58b995fb
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-03-21 08:47:25 -0300

    initial structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div
+      className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-green-500 to-blue-500
+    `}
+    >
+      <h1>Admin</h1>
+    </div>
+  )
+}
+
+export default Home
