6ecbb092ededf2388efd0a727c5612791f48d99f
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-03-25 14:26:17 -0300

    refactor: LAyout structure

--- src/components/template/Layout/index.tsx
+++ src/components/template/Layout/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
+import { Sidebar } from '../Sidebar'
 import { Content } from './Content'
 import { Header } from './Header'
-import { Sidebar } from './Sidebar'
 
 interface LayoutProps {
   title: string
